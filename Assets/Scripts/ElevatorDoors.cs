dd10246fde7d1f56c74f98a547160ab39a120673
andrewklundtFT <128536693+andrewklundtFT@users.noreply.github.com>
2024-02-17 03:06:53 -0600

    Added Sliding doors
    added sliding doors

---
+++ Assets/Scripts/ElevatorDoors.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoors : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 start;
+    public Vector3 center;
+    public bool isOpen;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        start = transform.GetChild(0).transform.position;
+        center = transform.GetChild(0).transform.position - transform.TransformDirection(new Vector3(0,0,2));
+    }
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 2)
+        {
+            transform.GetChild(0).transform.position = Vector3.Lerp(transform.GetChild(0).transform.position, center, 0.01f);
+            transform.GetChild(1).transform.position = Vector3.Lerp(transform.GetChild(1).transform.position, center, 0.01f);
+            isOpen = true;
+        } else
+        {
+            transform.GetChild(0).transform.position = Vector3.Lerp(transform.GetChild(0).transform.position, start, 0.01f);
+            transform.GetChild(1).transform.position = Vector3.Lerp(transform.GetChild(1).transform.position, start, 0.01f);
+            isOpen = false;
+        }
+    }
+}
